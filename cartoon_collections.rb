af4da4a2154011b1353929d8d471d570001a3e9c
Erica Little <eteney90@gmail.com>
2019-12-07 19:46:39 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,6 +20,6 @@ end
 def find_the_cheese(cheese)
   cheese_types = ["cheddar", "gouda", "camembert"]
   cheese.find do |cheese|
-  cheese.include? (cheese)
+  cheese_types.include? (cheese)
 end
 end
